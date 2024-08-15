ead933eae139474efa45b4c4c5574dec6a86008d
Nguyen Hoang Son <hellohoangson@gmail.com>
2024-08-16 00:15:29 +0700

    Deploy in vercel

--- src/api/ApiBase.js
+++ src/api/ApiBase.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const LOCAL_URL = "http://localhost:8085";
 export const BaseURL = "https://gadgetsource.click";
 const APIBase = axios.create({
-    baseURL: LOCAL_URL,
+    baseURL: BaseURL,
     withCredentials: true,
     headers: {
         credentials: 'include'
